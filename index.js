1062a19b3d4a84f77752e68466e6681a55c59515
Bryan S <superbee45@gmail.com>
2022-09-23 09:30:41 -0500

    shuffle them cards, Gary!

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ const myDeck =  {
       cards.push(this.drawCard());
     }
     return cards;
+  },
+  shuffle(){
+    const { deck } = this;
+    for (let i = deck.length -1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
   }
   
 }
