f287dc4feb320fbf329860247f0302b8cf0e9181
Bryan S <superbee45@gmail.com>
2022-09-22 20:32:37 -0500

    getting a deck to print out

---
+++ index.js
@@ -0,0 +1,11 @@
+function makeDeck() {
+  const deck = [];
+  const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
+  const values = '2,3,4,5,6,7,8,9,10,J,Q,K,A';
+  for(let value of values.split(',')) {
+    for(let suit of suits) {
+      deck.push({ value , suit})
+    }
+  }
+  return deck;
+}
