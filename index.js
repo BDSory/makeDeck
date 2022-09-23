2a3cf395d394a78c90af3c1bda221a419838f632
Bryan S <superbee45@gmail.com>
2022-09-23 09:10:14 -0500

    refactor and add drawCard

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function makeDeck() {
-  const deck = [];
-  const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
-  const values = '2,3,4,5,6,7,8,9,10,J,Q,K,A';
-  for(let value of values.split(',')) {
-    for(let suit of suits) {
-      deck.push({ value , suit})
+const myDeck =  {
+  deck : [],
+  suits : ['hearts', 'spades', 'diamonds', 'clubs'],
+  values : '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+  initializeDeck() {
+    const {suits, values, deck} = this;
+    for (let value of values.split(',')) {
+      for (let suit of suits) {
+        deck.push({value, suit})
+      }
     }
+  },
+  drawCard(){
+    return this.deck.pop();
   }
-  return deck;
+  
 }
